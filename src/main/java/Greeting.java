af0f05b4ab129843b15de1819e1d2da3389f17a6
yuryzkv <yuryzkv@gmail.com>
2018-11-13 00:43:04 +0700

    добавление Greeting

---
+++ src/main/java/Greeting.java
@@ -0,0 +1,5 @@
+public class Greeting {
+    public static void main(String...args){
+        System.out.println("Congratulation for listeners of course \"Professional developing on Java\"");
+    }
+}
