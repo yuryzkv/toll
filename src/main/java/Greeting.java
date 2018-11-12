b15c1d681a9f9c015fd35d93eb6a902b30d29606
yuryzkv <yuryzkv@gmail.com>
2018-11-13 00:45:54 +0700

    безжалостное удаление SRC

--- src/main/java/Greeting.java
+++
@@ -1,5 +0,0 @@
-public class Greeting {
-    public static void main(String...args){
-        System.out.println("Congratulation for listeners of course \"Professional developing on Java\"");
-    }
-}
