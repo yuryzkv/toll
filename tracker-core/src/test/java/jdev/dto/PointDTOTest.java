64e680f45e548bd67c29051b793e7fe47bc24256
yuryzkv <yuryzkv@gmail.com>
2018-11-13 03:44:34 +0700

    внутренняя зависимость

---
+++ tracker-core/src/test/java/jdev/dto/PointDTOTest.java
@@ -0,0 +1,34 @@
+package jdev.dto;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class PointDTOTest {
+
+    private String expected = "{\"lat\":56.0,\"lon\":74.0,\"autoId\":\"o567gfd\",\"time\":1542054037596}";
+    private String autoId = "o567gfd";
+
+    @Test
+    public void toJson() throws Exception {
+        PointDTO point = new PointDTO();
+        point.setLat(56);
+        point.setLon(74);
+        point.setAutoId("o567gfd");
+        point.setTime(System.currentTimeMillis());
+        assertTrue(point.toJson().contains("\"lat\":56"));
+        assertTrue(point.toJson().contains("\"time\":"));
+        System.out.println(point.toJson());
+    }
+
+    @Test
+    public void decodeDTO() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        PointDTO dto = mapper.readValue(expected, PointDTO.class);
+        assertEquals(autoId,dto.getAutoId());
+        assertEquals(1542054037596L,dto.getTime());
+
+    }
+}
