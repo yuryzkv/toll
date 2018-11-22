6b12bebf34b1587a29bd5930e6eb2ce80471708e
yuryzkv <yuryzkv@gmail.com>
2018-11-22 13:29:41 +0700

    добавлено чтение track файлов из kml-файлов

--- tracker-core/src/main/java/jdev/tracker/Main.java
+++ tracker-core/src/main/java/jdev/tracker/Main.java
@@ -2,14 +2,34 @@ package jdev.tracker;
 
 import jdev.dto.PointDTO;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String... args) throws Exception {
-        for (int i=0; i<5; i++) {
-            System.out.println("Main.main say Hello!!!!");
-            PointDTO point = new PointDTO();
-            point.setLat(45);
-            System.out.println(point.toJson());
-            Thread.sleep(1000);
+/*
+for (int i=0; i<5; i++) {
+System.out.println("Main.main say Hello!!!!");
+PointDTO point = new PointDTO();
+point.setLat(45);
+System.out.println(point.toJson());
+Thread.sleep(1000);
+}
+*/
+
+        List<File> files  = Files.walk(Paths.get("C:\\bin\\proj\\toll-services\\track-data\\"))
+                .filter(Files::isRegularFile)
+                .map(Path::toFile)
+                .collect(Collectors.toList());
+
+
+        for(File file : files){
+            System.out.println(file.getName());
         }
+
     }
 }
--- tracker-core/src/main/java/jdev/tracker/services/DataPeekService.java
+++ tracker-core/src/main/java/jdev/tracker/services/DataPeekService.java
@@ -1,8 +1,51 @@
 package jdev.tracker.services;
 
+import de.micromata.opengis.kml.v_2_2_0.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class DataPeekService {
 
-    public void readTrack(){
+    @Value("${trackPath.prop}")
+    public String trackPath;
+
+    @Scheduled(cron = "${cron.prop}")
+    public void readTrack() {
+        try {
+            List<File> files = Files.walk(Paths.get(trackPath),1)
+                    .filter(Files::isRegularFile)
+                    .filter(p -> p.toString().endsWith(".kml"))
+                    .map(Path::toFile)
+                    .collect(Collectors.toList());
+            for (File file : files) {
+                System.out.println("Trying to read kml file="+file.getName());
+                final Kml kml = Kml.unmarshal(file);
+                if (kml == null) {
+                    System.out.println("File " + file.getName() + " don't read");
+                    continue;
+                }
+                final Placemark placemark = (Placemark) kml.getFeature();
+                Point point = (Point) placemark.getGeometry();
+                List<Coordinate> coordinates = point.getCoordinates();
+                System.out.println("count of coordinate="+coordinates.size());
+                for (Coordinate coordinate : coordinates) {
+                    System.out.println("lat=" + coordinate.getLatitude() +
+                            " lon=" + coordinate.getLongitude() +
+                            " alt=" + coordinate.getAltitude());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
