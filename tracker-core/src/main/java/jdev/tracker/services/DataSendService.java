7412f1914d51374badba17532b85b87cac136a53
yuryzkv <yuryzkv@gmail.com>
2018-11-22 22:34:31 +0700

    выполнена реализация функций сервисов для получения,хранения,отправки координаты точки

--- tracker-core/src/main/java/jdev/tracker/services/DataSendService.java
+++ tracker-core/src/main/java/jdev/tracker/services/DataSendService.java
@@ -1,4 +1,23 @@
 package jdev.tracker.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jdev.dto.PointDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
 public class DataSendService {
+
+    @Autowired
+    DataStoreService dataStoreService;
+
+    @Scheduled(cron = "${cron.take}")
+    public void sendPoint() throws InterruptedException, JsonProcessingException {
+       PointDTO pointDTO = dataStoreService.getPoint();
+        ObjectMapper  mapper = new ObjectMapper();
+        String toJson = mapper.writeValueAsString(pointDTO);
+        System.out.println("Sending object pointDTO =>"+toJson);
+    }
 }
--- tracker-core/src/main/java/jdev/tracker/services/DataStoreService.java
+++ tracker-core/src/main/java/jdev/tracker/services/DataStoreService.java
@@ -1,4 +1,27 @@
 package jdev.tracker.services;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jdev.dto.PointDTO;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
+
+@Service
 public class DataStoreService {
+
+    private BlockingDeque<PointDTO> queue = new LinkedBlockingDeque<PointDTO>(100);
+
+    public void savePoint(String json) throws IOException, InterruptedException {
+        ObjectMapper mapper = new ObjectMapper();
+        PointDTO pointDTO = mapper.readValue(json,PointDTO.class);
+        queue.put(pointDTO);
+    }
+
+    public PointDTO getPoint() throws InterruptedException {
+        PointDTO pointDTO = queue.take();
+        return pointDTO;
+    }
 }
